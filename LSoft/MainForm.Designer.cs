df835baa82a01704a1da7de8f595f13ff20f96b8
Bruno Silva <lealbruno759@gmail.com>
2025-11-04 18:13:54 -0300

    Criação do Menu e dos Forms na pasta Views

--- LSoft/MainForm.Designer.cs
+++ LSoft/MainForm.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip1 = new MenuStrip();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripMenuItem();
+            toolStripMenuItem4 = new ToolStripMenuItem();
+            toolStripMenuItem5 = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, toolStripMenuItem2, toolStripMenuItem3, toolStripMenuItem4, toolStripMenuItem5 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(784, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "Menu";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(67, 20);
+            toolStripMenuItem1.Text = "Produtos";
+            toolStripMenuItem1.Click += toolStripMenuItem1_Click;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(61, 20);
+            toolStripMenuItem2.Text = "Clientes";
+            toolStripMenuItem2.Click += toolStripMenuItem2_Click;
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(56, 20);
+            toolStripMenuItem3.Text = "Vendas";
+            toolStripMenuItem3.Click += toolStripMenuItem3_Click;
+            // 
+            // toolStripMenuItem4
+            // 
+            toolStripMenuItem4.Name = "toolStripMenuItem4";
+            toolStripMenuItem4.Size = new Size(61, 20);
+            toolStripMenuItem4.Text = "Estoque";
+            toolStripMenuItem4.Click += toolStripMenuItem4_Click;
+            // 
+            // toolStripMenuItem5
+            // 
+            toolStripMenuItem5.Name = "toolStripMenuItem5";
+            toolStripMenuItem5.Size = new Size(71, 20);
+            toolStripMenuItem5.Text = "Relatórios";
+            toolStripMenuItem5.Click += toolStripMenuItem5_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 561);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "LSoft - Sistema de Supermercado";
+            Load += MainForm_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripMenuItem toolStripMenuItem3;
+        private ToolStripMenuItem toolStripMenuItem4;
+        private ToolStripMenuItem toolStripMenuItem5;
     }
 }
--- LSoft/MainForm.cs
+++ LSoft/MainForm.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Windows.Forms;
+
+// Importa os formulários que estão na pasta Views
+using LSoft.Views;
+
 namespace LSoft
 {
     public partial class MainForm : Form
@@ -5,6 +11,41 @@ namespace LSoft
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var form = new CadastroProdutoForm();
+            form.ShowDialog(); // Abre a janela de produtos
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            var form = new CadastroClienteForm();
+            form.ShowDialog(); // Abre modal
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            var form = new VendaForm();
+            form.ShowDialog(); // Abre modal
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            var form = new EstoqueForm();
+            form.ShowDialog(); // Abre modal
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            var form = new RelatorioForm();
+            form.ShowDialog(); // Abre modal
         }
     }
 }
