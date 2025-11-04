77f74f4022d0fa600d050e0f5444258e719ac37d
Bruno Silva <lealbruno759@gmail.com>
2025-11-04 19:23:44 -0300

    🛠️ Implementa CadastroProdutoForm completo com DataGridView funcional
    - Cria método CarregarProdutos() para preencher o grid a partir do banco SQLite
    - Ajusta cabeçalhos das colunas para nomes amigáveis
    - Oculta coluna Id e RowHeaders, remove linha de nova entrada (asterisco)
    - Configura DataGridView como somente leitura e seleção de linha completa
    - Ajusta AutoSizeColumnsMode com Fill e proporção (FillWeight) para colunas
    - Trata exceções na leitura do banco e mantém o grid limpo e res
    
    ponsivo

--- LSoft/Views/CadastroProdutoForm.Designer.cs
+++ LSoft/Views/CadastroProdutoForm.Designer.cs
@@ -28,19 +28,335 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            textBox6 = new TextBox();
+            label6 = new Label();
+            textBox7 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            checkBox1 = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            dgvProdutos = new DataGridView();
+            label11 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvProdutos).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nome:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(118, 19);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(357, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(118, 48);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(126, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 51);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Codigo de barras:";
+            label2.Click += label2_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(118, 77);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(126, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 80);
+            label3.Name = "label3";
+            label3.Size = new Size(90, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Código interno:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(118, 106);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(81, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 109);
+            label4.Name = "label4";
+            label4.Size = new Size(100, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Preço de compra:";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(324, 106);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(81, 23);
+            textBox5.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(205, 109);
+            label5.Name = "label5";
+            label5.Size = new Size(91, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Preço de venda:";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(118, 135);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(81, 23);
+            textBox6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 138);
+            label6.Name = "label6";
+            label6.Size = new Size(72, 15);
+            label6.TabIndex = 10;
+            label6.Text = "Quantidade:";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(324, 135);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(81, 23);
+            textBox7.TabIndex = 13;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(205, 138);
+            label7.Name = "label7";
+            label7.Size = new Size(113, 15);
+            label7.TabIndex = 12;
+            label7.Text = "Unidade de medida:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(12, 166);
+            label8.Name = "label8";
+            label8.Size = new Size(70, 15);
+            label8.TabIndex = 14;
+            label8.Text = "Fornecedor:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(205, 166);
+            label9.Name = "label9";
+            label9.Size = new Size(61, 15);
+            label9.TabIndex = 15;
+            label9.Text = "Categoria:";
+            label9.Click += label9_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(12, 198);
+            label10.Name = "label10";
+            label10.Size = new Size(97, 15);
+            label10.TabIndex = 16;
+            label10.Text = "Data de validade:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(118, 163);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(81, 23);
+            comboBox1.TabIndex = 18;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(324, 163);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(81, 23);
+            comboBox2.TabIndex = 19;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(118, 192);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(287, 23);
+            dateTimePicker1.TabIndex = 20;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(491, 21);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(89, 19);
+            checkBox1.TabIndex = 21;
+            checkBox1.Text = "Status Ativo";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 243);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 22;
+            button1.Text = "Salvar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(93, 243);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 23;
+            button2.Text = "Editar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(174, 243);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 24;
+            button3.Text = "Excluir";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(255, 243);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 25;
+            button4.Text = "Cancelar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // dgvProdutos
+            // 
+            dgvProdutos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProdutos.Location = new Point(12, 302);
+            dgvProdutos.Name = "dgvProdutos";
+            dgvProdutos.Size = new Size(760, 247);
+            dgvProdutos.TabIndex = 26;
+            dgvProdutos.CellContentClick += dgvProdutos_CellContentClick;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label11.Location = new Point(324, 284);
+            label11.Name = "label11";
+            label11.Size = new Size(131, 15);
+            label11.TabIndex = 27;
+            label11.Text = " Produtos Cadastrados";
             // 
             // CadastroProdutoForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.HotTrack;
             ClientSize = new Size(784, 561);
+            Controls.Add(label11);
+            Controls.Add(dgvProdutos);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(textBox7);
+            Controls.Add(label7);
+            Controls.Add(textBox6);
+            Controls.Add(label6);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
             Name = "CadastroProdutoForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Cadastro de Produtos";
+            Text = " ";
+            Load += CadastroProdutoForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvProdutos).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label5;
+        private TextBox textBox6;
+        private Label label6;
+        private TextBox textBox7;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private DateTimePicker dateTimePicker1;
+        private CheckBox checkBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private DataGridView dgvProdutos;
+        private Label label11;
     }
 }
--- LSoft/Views/CadastroProdutoForm.cs
+++ LSoft/Views/CadastroProdutoForm.cs
@@ -1,20 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
 
 namespace LSoft.Views
 {
     public partial class CadastroProdutoForm : Form
     {
+        private string connectionString;
+
         public CadastroProdutoForm()
         {
             InitializeComponent();
+
+            // Cria o caminho dinâmico do banco na pasta DataAccess
+            string pastaBanco = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DataAccess");
+            string caminhoBanco = Path.Combine(pastaBanco, "supermercado.db");
+            connectionString = $"Data Source={caminhoBanco}";
+
+            // Associa o evento Load do formulário
+            this.Load += CadastroProdutoForm_Load;
+        }
+
+        // Evento Load do formulário
+        private void CadastroProdutoForm_Load(object? sender, EventArgs e)
+        {
+            CarregarProdutos();
+        }
+
+        // Método que carrega os produtos no DataGridView
+        private void CarregarProdutos()
+        {
+            try
+            {
+                using (var conn = new SqliteConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = @"
+SELECT 
+    Id,                  -- pode ser oculto no grid, usado internamente
+    Nome,                -- nome do produto
+    PrecoVenda,          -- preço de venda
+    QuantidadeEmEstoque, -- estoque atual
+    CategoriaId,         -- você pode mostrar o nome da categoria depois via JOIN
+    FornecedorId,        -- você pode mostrar o nome do fornecedor via JOIN
+    DataValidade         -- para produtos perecíveis
+FROM Produto
+WHERE StatusAtivo = 1  -- apenas produtos ativos
+ORDER BY Nome;         -- organiza por nome para facilitar a visualização
+";
+
+                    using (var cmd = new SqliteCommand(query, conn))
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(reader); // Carrega o DataReader direto no DataTable
+
+                        dgvProdutos.DataSource = dt;
+
+                        // Configura cabeçalhos e oculta ID
+                        dgvProdutos.Columns["Nome"].HeaderText = "Nome do Produto";
+                        dgvProdutos.Columns["PrecoVenda"].HeaderText = "Preço de Venda";
+                        dgvProdutos.Columns["QuantidadeEmEstoque"].HeaderText = "Estoque";
+                        dgvProdutos.Columns["CategoriaId"].HeaderText = "Categoria";
+                        dgvProdutos.Columns["FornecedorId"].HeaderText = "Fornecedor";
+                        dgvProdutos.Columns["DataValidade"].HeaderText = "Validade";
+                        dgvProdutos.Columns["Id"].Visible = false;
+                        dgvProdutos.AllowUserToAddRows = false;
+                        dgvProdutos.RowHeadersVisible = false;
+                        dgvProdutos.ReadOnly = true;
+                        dgvProdutos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                        dgvProdutos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar produtos: " + ex.Message);
+            }
+        }
+
+        // Eventos de labels (se quiser manter)
+        private void label2_Click(object sender, EventArgs e) { }
+        private void label9_Click(object sender, EventArgs e) { }
+
+        private void dgvProdutos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
